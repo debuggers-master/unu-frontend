21aba4994a0f20033cfed523a5fa023b1fdb9997
devacran <migueeldrg@gmail.com>
2020-08-18 17:37:43 -0500

    MODIFY style config

--- src/config.dev.js
+++ src/config.dev.js
@@ -1,3 +1,3 @@
 module.exports = {
-  API_URL: ""
-};
+  API_URL: ''
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles/globals.scss";
+import "./styles/main.scss";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
