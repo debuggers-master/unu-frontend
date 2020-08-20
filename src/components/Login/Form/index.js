ba24dd5d064d44dd3e89ccf34627f1df3e678faa
devacran <migueeldrg@gmail.com>
2020-08-20 18:44:07 -0500

    Modify signup form

--- src/components/Login/Form/index.js
+++ src/components/Login/Form/index.js
@@ -1,47 +1,15 @@
 /* Issue: when a input field is selected by tabs */
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 
 const Form = () => {
-  const [clickedField, setClickedField] = useState()
-
-  const handleOutsideClick = evn => {
-    if (clickedField) {
-      const label = clickedField.getElementsByTagName('label')[0]
-      const input = clickedField.getElementsByTagName('input')[0]
-      /* eslint-disable */
-      const isInactive = evn.target != input
-      /* eslint-enable */
-      const isFill = input.value
-      if (isInactive && !isFill) {
-        label.classList.remove('form-field__label--active')
-      }
-    }
-  }
-  useEffect(() => {
-    window.addEventListener('click', handleOutsideClick)
-  }, [handleOutsideClick])
-
-  const handleClick = evn => {
-    const formField = evn.currentTarget
-    setClickedField(formField)
-    const activeLabel = formField.getElementsByTagName('label')[0]
-    activeLabel.classList.add('form-field__label--active')
-  }
-  const handleChange = evn => {
-    console.log('change')
-  }
   return (
     <>
       <form>
-        <div onClick={handleClick} className='form-field'>
+        <div className='form-field'>
           <label className='form-field__label'>Email</label>
           <input type='text' name='email' />
         </div>
-        <div
-          onClick={handleClick}
-          onChange={handleChange}
-          className='form-field'
-        >
+        <div className='form-field'>
           <label className='form-field__label'>Contraseña</label>
           <input type='text' name='' />
         </div>
--- src/components/Signup/Form/index.js
+++ src/components/Signup/Form/index.js
@@ -3,48 +3,44 @@
 import React, { useState, useEffect } from 'react'
 
 const Form = () => {
-  console.log('componente renderizado')
-
-  const [clickedField, setClickedField] = useState()
   const [modifiedFields, setModifiedFields] = useState({})
-  const [errors, setErrors] = useState({})
-
-  const handleOutisdeClick = evn => {
-    if (clickedField) {
-      console.log('click')
-      const label = clickedField.getElementsByTagName('label')[0]
-      const input = clickedField.getElementsByTagName('input')[0]
-      /* eslint-disable */
-      const isInactive = evn.target != input
-      /* eslint-enable */
-      const isFill = input.value
-      if (isInactive && !isFill) {
-        label.classList.remove('form-field__label--active')
-        validateFields(modifiedFields)
-      }
-    }
-  }
-
-  useEffect(() => {
-    document.addEventListener('click', handleOutisdeClick)
-  }, [handleOutisdeClick])
-
+  const [errors, setErrors] = useState({
+    email: '',
+    password: '',
+    name: ''
+  })
+  const [enableSubmit, setEnableSubmit] = useState()
   const handleClick = evn => {
     const formField = evn.currentTarget
-    setClickedField(formField)
     const activeLabel = formField.getElementsByTagName('label')[0]
     activeLabel.classList.add('form-field__label--active')
+    formField.classList.add('form-field--active')
   }
   const handleChange = evn => {
-    console.log('handleChange')
     const fieldName = evn.target.name
     const fieldValue = evn.target.value
+    console.log(fieldName)
     setModifiedFields({ ...modifiedFields, [fieldName]: fieldValue })
   }
-
+  const handleBur = evn => {
+    const field = evn.currentTarget
+    const label = field.getElementsByTagName('label')[0]
+    const input = field.getElementsByTagName('input')[0]
+    const isFill = input.value
+    if (!isFill) {
+      label.classList.remove('form-field__label--active')
+      field.classList.remove('form-field--active')
+    }
+    validateFields(modifiedFields)
+  }
+  const handleSubmit = () => {}
   const validateFields = fields => {
     const validators = {
-      email: emailValidator
+      email: emailValidator,
+      name: nameValidator,
+      lastName: nameValidator,
+      password: passwordValidator,
+      passwordConfirm: passwordConfirmValidator
     }
     for (const type in fields) {
       const value = fields[type]
@@ -54,39 +50,103 @@ const Form = () => {
       }
     }
   }
+  useEffect(() => {
+    setEnableSubmit(formValidator(errors))
+  }, [errors])
+
+  const formValidator = errors => {
+    for (const error in errors) {
+      if (errors[error] !== null) {
+        return false
+      }
+    }
+    return true
+  }
   const emailValidator = email => {
-    console.log(email)
-    console.log('validando...')
-    return 'tienes que poner un email'
+    if (/^[\w-.]+@([\w-]+.)+[\w-]{2,4}$/.test(email)) {
+      return null
+    }
+    if (email.trim() === '') {
+      return 'Ingresa un email'
+    }
+    return 'Ingresa un email valido'
+  }
+  const nameValidator = name => {
+    if (name.trim() === '') {
+      return 'Ingresa un nombre'
+    }
+    return null
+  }
+  const passwordValidator = password => {
+    if (/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password)) {
+      return null
+    }
+    if (password.trim() === '') {
+      return 'Ingresa una contraseña'
+    }
+    return 'Ingresa una contraseña correcta'
+  }
+  const passwordConfirmValidator = pass => {
+    if (pass === modifiedFields.password) {
+      return null
+    }
+    return 'Las contraseñas no son iguales'
   }
   return (
     <>
-      <form>
-        <div onClick={handleClick} className='form-field'>
-          <label className='form-field__label'>Name</label>
-          <input type='text' name='' />
+      <form onSubmit={handleSubmit} autoComplete='off'>
+        <div onBlur={handleBur} onClick={handleClick} className='form-field'>
+          <label className='form-field__label'>Nombre</label>
+          <input onChange={handleChange} type='text' name='name' />
+          {errors.name && (
+            <div className='form-field__error'>{errors.name}</div>
+          )}
         </div>
-        <div onClick={handleClick} className='form-field'>
-          <label className='form-field__label'>Last Name</label>
-          <input type='text' name='' />
+        <div onBlur={handleBur} onClick={handleClick} className='form-field'>
+          <label className='form-field__label'>Apellido</label>
+          <input onChange={handleChange} type='text' name='lastName' />
+          {errors.lastName && (
+            <div className='form-field__error'>{errors.lastName}</div>
+          )}
         </div>
-        <div onClick={handleClick} className='form-field'>
+
+        <div
+          onBlur={handleBur}
+          onClick={handleClick}
+          className={
+            errors.email ? 'form-field form-field--error' : 'form-field'
+          }
+        >
           <label className='form-field__label'>Email</label>
           <input onChange={handleChange} type='text' name='email' />
           {errors.email && (
             <div className='form-field__error'>{errors.email}</div>
           )}
         </div>
-        <div onClick={handleClick} className='form-field'>
+        <div onBlur={handleBur} onClick={handleClick} className='form-field'>
           <label className='form-field__label'>Contraseña</label>
-          <input type='text' name='' />
+          <input onChange={handleChange} type='password' name='password' />
+          {errors.password && (
+            <div className='form-field__error'>{errors.password}</div>
+          )}
         </div>
-        <div onClick={handleClick} className='form-field'>
+        <div onBlur={handleBur} onClick={handleClick} className='form-field'>
           <label className='form-field__label'>Confirmar Contraseña</label>
-          <input type='text' name='' />
+          <input
+            onChange={handleChange}
+            type='password'
+            name='passwordConfirm'
+          />
+          {errors.passwordConfirm && (
+            <div className='form-field__error'>{errors.passwordConfirm}</div>
+          )}
         </div>
         <div className='signup__button'>
-          <button className='button' type='submit'>
+          <button
+            disabled={!enableSubmit}
+            className={enableSubmit ? 'button' : 'button button--disabled'}
+            type='submit'
+          >
             Registrate
           </button>
         </div>
