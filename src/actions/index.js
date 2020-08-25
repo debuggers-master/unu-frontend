edb6cbf8dad5f74fbef993497085562019d3e541
Daniel Valderrama <dny9919@gmail.com>
2020-08-25 14:20:04 -0500

    UPDATE in actions changes in api

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,19 +13,31 @@ export const redirect = payload => ({
   type: 'REDIRECT_TO_URL',
   payload
 })
+export const signError = payload => ({
+  type: 'SIGN_ERROR',
+  payload
+})
 
-export const registerUser = payload => {
+export const registerUser = (payload, redirectUrl) => {
   return async dispatch => {
     try {
-      await axios({
+      const { data } = await axios({
         url: `${API_URL}/auth/signup`,
         method: 'post',
         data: payload
       })
-      window.location.href = '/login'
-      // dispatch(loginRequest(data.data.user))
+      dispatch(registerRequest(data.user))
+      document.cookie = `token=${data.access_token}`
+      document.cookie = `userID=${data.user.userId}`
+      /*eslint-disable */
+      sessionStorage.setItem('myData', JSON.stringify(data.user))
+      /* eslint-enable */
+      dispatch(signError(null))
+      window.location.href = redirectUrl
     } catch (error) {
       console.log(error)
+      error.response.status === 409 &&
+        dispatch(signError('Usario ya registrado'))
     }
   }
 }
@@ -39,50 +51,22 @@ export const loginUser = (payload, redirectUrl) => {
       })
       dispatch(loginRequest(data.user))
       document.cookie = `token=${data.access_token}`
-      document.cookie = `userID=${data.user.user_id}`
+      document.cookie = `userID=${data.user.userId}`
       /*eslint-disable */
       sessionStorage.setItem('myData', JSON.stringify(data.user))
       /* eslint-enable */
       window.location.href = redirectUrl
     } catch (error) {
       console.log(error)
+      error.response.status === 401 && dispatch(signError('Datos incorrectos'))
     }
   }
 }
-// export const newEvent = payload => {
-//   return async dispatch => {
-//     try {
-//       await axios({
-//         url: `${API_URL}/events`,
-//         method: 'post',
-//         data: payload
-//       })
-//       window.location.href = '/NewEvent'
-//     } catch (error) {
-//       console.log(error)
-//     }
-//   }
-// }
-
-// export const removeCollaborate = payload => {
-//   return async dispatch =>{
-//     try{
-//       await axios({
-//         url: `${API_URL}/events/collaborators`,
-//         method: 'delete',
-//         data: payload
-//       })
-//       window.location.href = './events/edit/1'
-//     } catch (error){
-//       console.log(error);
-//     }
-//   }
-// }
 // "user": {
 //     "email": "name_lasta@organization.com",
 //     "firstName": "Mario",
 //     "lastName": "Barbosa",
-//     "user_id": "caf1e98a-c84f-4a8e-9f88-81f6ab3db4a8",
+//     "userId": "caf1e98a-c84f-4a8e-9f88-81f6ab3db4a8",
 //     "organizations": [],
 //     "collaborations": []
 //   }
