09441f39f22187232199f3a8a5b0729d35394742
Daniel Valderrama <dny9919@gmail.com>
2020-08-25 12:06:46 -0500

    RESOLVE conflict

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,31 +13,19 @@ export const redirect = payload => ({
   type: 'REDIRECT_TO_URL',
   payload
 })
-export const signError = payload => ({
-  type: 'SIGN_ERROR',
-  payload
-})
 
-export const registerUser = (payload, redirectUrl) => {
+export const registerUser = payload => {
   return async dispatch => {
     try {
-      const { data } = await axios({
+      await axios({
         url: `${API_URL}/auth/signup`,
         method: 'post',
         data: payload
       })
-      dispatch(registerRequest(data.user))
-      document.cookie = `token=${data.access_token}`
-      document.cookie = `userID=${data.user.user_id}`
-      /*eslint-disable */
-      sessionStorage.setItem('myData', JSON.stringify(data.user))
-      /* eslint-enable */
-      dispatch(signError(null))
-      window.location.href = redirectUrl
+      window.location.href = '/login'
+      // dispatch(loginRequest(data.data.user))
     } catch (error) {
       console.log(error)
-      error.response.status === 409 &&
-        dispatch(signError('Usario ya registrado'))
     }
   }
 }
@@ -58,10 +46,38 @@ export const loginUser = (payload, redirectUrl) => {
       window.location.href = redirectUrl
     } catch (error) {
       console.log(error)
-      error.response.status === 401 && dispatch(signError('Datos incorrectos'))
     }
   }
 }
+// export const newEvent = payload => {
+//   return async dispatch => {
+//     try {
+//       await axios({
+//         url: `${API_URL}/events`,
+//         method: 'post',
+//         data: payload
+//       })
+//       window.location.href = '/NewEvent'
+//     } catch (error) {
+//       console.log(error)
+//     }
+//   }
+// }
+
+// export const removeCollaborate = payload => {
+//   return async dispatch =>{
+//     try{
+//       await axios({
+//         url: `${API_URL}/events/collaborators`,
+//         method: 'delete',
+//         data: payload
+//       })
+//       window.location.href = './events/edit/1'
+//     } catch (error){
+//       console.log(error);
+//     }
+//   }
+// }
 // "user": {
 //     "email": "name_lasta@organization.com",
 //     "firstName": "Mario",
